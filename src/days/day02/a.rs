a0537eb18e1e8949908c2db8d6c327c768b3dcf4
Francesco Noacco <francesco.noacco@secomind.com>
2023-12-02 19:32:59 +0100

    feat: 2a
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- src/days/day02/a.rs
+++ src/days/day02/a.rs
@@ -1,5 +1,31 @@
+use super::utils;
+use utils::GameStatus;
+
 #[allow(dead_code)]
 #[allow(unused_variables)]
 pub fn solve(input: &str) -> String {
-    panic!("Not implemented yet")
+    let maximums = [("red", 12), ("green", 13), ("blue", 14)];
+    input
+        .lines()
+        .filter_map(|line| match utils::check_game_validity(line, &maximums) {
+            GameStatus::Valid(id) => Some(id),
+            GameStatus::Invalid(_) => None,
+        })
+        .sum::<u128>()
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn sample() {
+        let sample = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let result = solve(sample);
+        assert_eq!(result, "8");
+    }
 }
--- src/days/day02/utils.rs
+++ src/days/day02/utils.rs
@@ -1,3 +1,44 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
 pub fn read_input() -> &'static str {
     include_str!("input")
+}
+
+pub enum GameStatus {
+    Valid(u128),
+    Invalid(u128),
+}
+
+lazy_static! {
+    static ref GAME_ID_RE: Regex = Regex::new(r"^\s*Game (\d+):").unwrap();
+}
+
+pub fn check_game_validity(game: &str, maximums: &[(&str, u128)]) -> GameStatus {
+    let game_id = GAME_ID_RE
+        .captures(game)
+        .unwrap()
+        .get(1)
+        .unwrap()
+        .as_str()
+        .parse::<u128>()
+        .unwrap();
+
+    match maximums
+        .iter()
+        .map(|(color, max)| (all_color_values(game, color), max))
+        .all(|(values, max)| values.iter().all(|x| x <= max))
+    {
+        true => GameStatus::Valid(game_id),
+        false => GameStatus::Invalid(game_id),
+    }
+}
+
+fn all_color_values<'a>(game: &'a str, color: &'a str) -> Vec<u128> {
+    let color = format!("(\\d+) {color}");
+    Regex::new(&color)
+        .unwrap()
+        .captures_iter(game)
+        .map(|c| c.get(1).unwrap().as_str().parse::<u128>().unwrap())
+        .collect()
 }
