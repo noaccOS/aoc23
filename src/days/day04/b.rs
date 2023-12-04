40eb201c9a944a64a5d43d1ba001859679d588ae
Francesco Noacco <francesco.noacco@secomind.com>
2023-12-04 23:51:22 +0100

    feat: prepare 4b
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- src/days/day04/b.rs
+++ src/days/day04/b.rs
@@ -13,6 +13,6 @@ mod tests {
     fn sample() {
         let sample = include_str!("sample_b");
         let result = solve(sample);
-        assert_eq!(result, "");
+        assert_eq!(result, "30");
     }
 }
