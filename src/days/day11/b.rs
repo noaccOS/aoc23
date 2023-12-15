d8d8ac7484273b83276f13710db3e21536172508
Francesco Noacco <francesco.noacco2000@gmail.com>
2023-12-15 15:26:17 +0100

    feat: 11b
    Signed-off-by: Francesco Noacco <francesco.noacco2000@gmail.com>

--- src/days/day11/b.rs
+++ src/days/day11/b.rs
@@ -1,21 +1,30 @@
 #[allow(unused_imports)]
 use super::utils::*;
+use itertools::Itertools;
 
 #[allow(dead_code)]
 #[allow(unused_variables)]
 pub fn solve(input: &str) -> String {
-    panic!("Not implemented yet")
+    let empty_space = 1000000 - 1;
+    let space = Space::from_input(input, empty_space);
+    space
+        .galaxies
+        .iter()
+        .combinations(2)
+        .map(|galaxies| space.distance(&galaxies[0], &galaxies[1]))
+        .sum::<usize>()
+        .to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[ignore]
+    //#[ignore]
     #[test]
     fn sample() {
-        let sample = include_str!("sample_b");
+        let sample = include_str!("sample_a");
         let result = solve(sample);
-        assert_eq!(result, "");
+        assert_eq!(result, "1030");
     }
 }
