d46b3be7c20f822499ec3d68df8698d3c92d7bd7
Francesco Noacco <francesco.noacco2000@gmail.com>
2023-12-17 23:26:38 +0100

    feat: 12b
    Signed-off-by: Francesco Noacco <francesco.noacco2000@gmail.com>

--- src/days/day12/b.rs
+++ src/days/day12/b.rs
@@ -4,18 +4,17 @@ use super::utils::*;
 #[allow(dead_code)]
 #[allow(unused_variables)]
 pub fn solve(input: &str) -> String {
-    panic!("Not implemented yet")
+    input.lines().map(|line| Line::new_long(line).arrangements()).sum::<usize>().to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[ignore]
     #[test]
     fn sample() {
         let sample = include_str!("sample_b");
         let result = solve(sample);
-        assert_eq!(result, "");
+        assert_eq!(result, "525152");
     }
 }
--- src/days/day12/utils.rs
+++ src/days/day12/utils.rs
@@ -20,10 +20,13 @@ impl Character {
     }
 }
 
+use std::collections::HashMap;
+
 #[derive(Debug, Clone)]
 pub struct Line {
     input: Vec<Character>,
     sections: Vec<usize>,
+    cache: HashMap<(isize, isize, usize), usize>,
 }
 
 impl Line {
@@ -42,10 +45,38 @@ impl Line {
             .split(',')
             .map(|s| s.parse::<usize>().unwrap())
             .collect();
-        Self { input, sections }
+        let cache = HashMap::new();
+        Self {
+            input,
+            sections,
+            cache,
+        }
     }
 
-    pub fn arrangements(&self) -> usize {
+    pub fn new_long(input: &str) -> Self {
+        let new_short = Self::new(input);
+        let mut input = new_short.input.clone();
+        input.reserve(input.len() * 4 + 4);
+        for _ in 0..4 {
+            input.push(Character::Undefined);
+            input.extend_from_slice(&new_short.input);
+        }
+        let sections = new_short
+            .sections
+            .iter()
+            .cycle()
+            .take(new_short.sections.len() * 5)
+            .map(|x| *x)
+            .collect();
+
+        Self {
+            input,
+            sections,
+            cache: new_short.cache,
+        }
+    }
+
+    pub fn arrangements(&mut self) -> usize {
         let last_idx = self.input.len() as isize - 1;
         let last_group = self.sections.len() as isize - 1;
         let initial_group_size = 0;
@@ -53,17 +84,22 @@ impl Line {
     }
 
     fn _arrangements(
-        &self,
+        &mut self,
         idx: isize,
         current_group_idx: isize,
         current_group_size: usize,
     ) -> usize {
+        if let Some(&n) = self.cache.get(&(idx, current_group_idx, current_group_size)) {
+            return n;
+        }
+
         let group_size = match usize::try_from(current_group_idx) {
             Ok(n) => self.sections[n],
             _ => 0,
         };
 
         if current_group_size > group_size {
+            self.cache.insert((idx, current_group_idx, current_group_size), 0);
             return 0;
         }
 
@@ -78,25 +114,33 @@ impl Line {
         let next = idx - 1;
         match self.input[idx as usize] {
             Character::Present => {
-                self._arrangements(next, current_group_idx, current_group_size + 1)
+                let cached = self._arrangements(next, current_group_idx, current_group_size + 1);
+                self.cache.insert((idx, current_group_idx, current_group_size), cached);
+                cached
             }
             Character::Missing => {
-                self.check_missing(next, current_group_idx, current_group_size, group_size)
+                let cached = self.check_missing(next, current_group_idx, current_group_size, group_size);
+                self.cache.insert((idx, current_group_idx, current_group_size), cached);
+                cached
             }
-            Character::Undefined => match current_group_size {
-                n if n == group_size => self._arrangements(next, current_group_idx - 1, 0),
-                n => {
-                    let as_missing =
-                        self.check_missing(next, current_group_idx, current_group_size, group_size);
-                    let as_present = self._arrangements(next, current_group_idx, n + 1);
-                    as_missing + as_present
-                }
+            Character::Undefined => {
+                let cached = match current_group_size {
+                    n if n == group_size => self._arrangements(next, current_group_idx - 1, 0),
+                    n => {
+                        let as_missing =
+                            self.check_missing(next, current_group_idx, current_group_size, group_size);
+                        let as_present = self._arrangements(next, current_group_idx, n + 1);
+                        as_missing + as_present
+                    }
+                };
+                self.cache.insert((idx, current_group_idx, current_group_size), cached);
+                cached
             },
         }
     }
 
     fn check_missing(
-        &self,
+        &mut self,
         next: isize,
         current_group_idx: isize,
         current_group_size: usize,
